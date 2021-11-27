a47702e8f5cb2b66c3cfd6d0f1fbcc21993fda74
nrgbistro <nolangelinas@gmail.com>
2021-11-26 20:48:17 -0500

    Rename variables and consolitade files

--- contracts/ProposalList.sol
+++ contracts/ProposalList.sol
@@ -1,63 +1,94 @@
 pragma solidity >=0.5.16 <0.9.0;
-import "./Governance.sol";
 
 contract ProposalList {
 
-struct Proposal{
-    uint proposalID;
-    string proposalName;
-    address proposalCreator;
-    string proposalSummary;
-    Governance[] optionList;
+    struct Proposal{
+        uint id;
+        string name;
+        address creator;
+        string summary;
+        Option[] optionList;
+    }
+    uint propCount = 0;
+    mapping (uint => Proposal) proposals;
+    Proposal[] public propList;
+    mapping (uint => address) proposalCreators;
+
+
+    constructor() public{
+        addProposal(0,"Game Types", msg.sender, "What type of game");
+        propCount = 0;
+        //proposals[0] = Proposal(0, "Click-based Game", 0, currentState, "It's a click-based game");
+        //proposals[1] = Proposal(1, "Keyboard-based game", 0,  currentState, "wearLevel2");
+        addProposal(0, "Game Type", msg.sender,"What kind of game do you prefer");
+
+        propList.push(proposals[0]);
+        propList.push(proposals[1]);
+    }
+
+    function addProposal (uint _id, string memory _name, address _creator, string memory _summary) public {
+        Governance[] memory temp;
+        proposals[propCount] = Proposal(_id, _name , _creator, _summary,temp);
+        propCount ++;
+    }
+
+    function getName(uint place) public view returns(string memory) {
+        return propList[place].name;
+    }
+
+    function getOptionName(uint place, uint index) public view returns(string memory){
+        return propList[place].optionList[index].getName();
+    }
+
+    function getOptionVoteCount(uint place, uint index) public view returns(uint) {
+        return propList[place].optionList[index].getVoteCount();
+    }
+
+    function getCreator(uint place) public view returns(address) {
+        return propList[place].creator;
+    }
+
+    function getOptionCreator(uint place, uint index) public view returns(address) {
+        return propList[place].optionList[index].getCreator();
+    }
+
+    function getOptionSummary(uint place, uint index) public view returns(string memory) {
+        return propList[place].optionList[index].getSummary();
+    }
+
+    function getSummary(uint place) public view returns(string memory) {
+        return propList[place].summary;
+    }
 }
-uint propCount = 0;
-mapping (uint => Proposal) struct2;
-Proposal[] public propList;
-mapping (uint => address) proposalCreators;
 
+contract Option {
+    uint id;
+    string name;
+    uint voteCount;
+    address creator;
+    string summary;
 
-constructor() public{
-    addProposal(0,"Game Types", msg.sender, "What type of game");
-    propCount = 0;
-    //struct2[0] = Proposal(0, "Click-based Game", 0, currentState, "It's a click-based game");
-    //struct2[1] = Proposal(1, "Keyboard-based game", 0,  currentState, "wearLevel2");
-    addProposal(0, "Game Type", msg.sender,"What kind of game do you prefer");
+    mapping (uint => Option) options;
 
-    propList.push(struct2[0]);
-    propList.push(struct2[1]);
+    Option[] public optionDataArray;
+
+    constructor(uint _id, string memory _name, string memory _summary) public {
+        id = _id;
+        name = _name;
+        voteCount = 0;
+        creator = msg.sender;
+        summary = _summary;
+    }
+    function getName() public view returns(string memory) {
+        return name;
+    }
+    function getVoteCount() public view returns(uint) {
+        return voteCount;
+    }
+    function getCreator() public view returns(address) {
+        return creator;
+    }
+    function getSummary() public view returns(string memory) {
+        return summary;
+    }
 }
-
-function addProposal (uint _id, string memory _name, address _creator, string memory _summary) public {
-    Governance[] memory temp;
-    struct2[propCount] = Proposal(_id, _name , _creator, _summary,temp);
-    propCount ++;
-}
-
-function getName(uint place) public view returns(string memory) {
-    return propList[place].proposalName;
-}
-
-function getOptionName(uint place, uint index) public view returns(string memory){
-    return propList[place].optionList[index].getName();
-}
-
-function getOptionVoteCount(uint place, uint index) public view returns(uint) {
-    return propList[place].optionList[index].getVoteCount();
-}
-
-function getCreator(uint place) public view returns(address) {
-    return propList[place].proposalCreator;
-}
-
-function getOptionCreator(uint place, uint index) public view returns(address) {
-    return propList[place].optionList[index].getCreator();
-}
-
-function getOptionSummary(uint place, uint index) public view returns(string memory) {
-    return propList[place].optionList[index].getSummary();
-}
-
-function getSummary(uint place) public view returns(string memory) {
-    return propList[place].proposalSummary;
-}
-} 
