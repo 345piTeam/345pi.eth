6983c2d43c6e4f77cba8aeaeac931f0ba6ba2ca4
Aryan-Chaudhary0220 <aryan.chaudhary@duke.edu>
2021-11-26 20:57:02 -0500

    fixed migrations

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,11 @@
 const SimpleStorage = artifacts.require("SimpleStorage");
 const ComplexStorage = artifacts.require("ComplexStorage");
 const Option = artifacts.require("Option");
+const ProposalList = artifacts.require("ProposalList");
 
 module.exports = function (deployer) {
 	deployer.deploy(SimpleStorage);
 	deployer.deploy(ComplexStorage);
 	deployer.deploy(Option, 0, "Test", "Test");
+	deployer.deploy(ProposalList);
 };
