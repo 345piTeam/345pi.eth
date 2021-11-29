0e733f75dd7f0d48386a18e450afd0f9689eeab7
Nolan Gelinas <nolangelinas@gmail.com>
2021-11-29 16:45:47 -0500

    Add background image and character icons

--- app/src/pages/Homepage.js
+++ app/src/pages/Homepage.js
@@ -1,12 +1,18 @@
 import styles from "./css/Homepage.module.css";
 import React from "react";
 import darkLord from "../images/characters/Darklord.png";
+import wizard from "../images/characters/Wizard.png";
+import pawn from "../images/characters/Pawn.png";
+import dukeOG from "../images/characters/Duke OG.png";
+import princess from "../images/characters/Princess.png";
 import { Avatar, Col, Row } from "antd";
 import {
 	WalletOutlined,
 	LineChartOutlined,
 	ExperimentOutlined,
 } from "@ant-design/icons";
+
+const characterDescriptionSpanSize = 7;
 
 const Homepage = () => (
 	<div className={styles.mainContainer}>
@@ -38,12 +44,45 @@ const Homepage = () => (
 			</Row>
 		</div>
 		<div className={styles.characterDescriptionContainer}>
-			<div className={styles.characterDescription}>
-				<Avatar src={darkLord} id={styles.darkLord} shape="circle" size={64} />
-				<div className={styles.description}>
-					<b>Pawn:</b> description
-				</div>
-			</div>
+			<Row justify="space-around">
+				<Col span={characterDescriptionSpanSize}>
+					<CharacterCell image={pawn} name={"Pawn"} description={"desc"} />
+				</Col>
+				<Col span={characterDescriptionSpanSize}>
+					<CharacterCell image={dukeOG} name={"Duke OG"} description={"desc"} />
+				</Col>
+				<Col span={characterDescriptionSpanSize}>
+					<CharacterCell
+						image={princess}
+						name={"Royalty"}
+						description={"desc"}
+					/>
+				</Col>
+			</Row>
+			<Row justify="center" gutter={36}>
+				<Col span={characterDescriptionSpanSize + 1}>
+					<CharacterCell
+						image={darkLord}
+						name={"Dark Lord"}
+						description={"desc"}
+					/>
+				</Col>
+				<Col span={characterDescriptionSpanSize + 1}>
+					<CharacterCell image={wizard} name={"Wizard"} description={"desc"} />
+				</Col>
+			</Row>
+		</div>
+	</div>
+);
+
+const CharacterCell = ({ image, name, description }) => (
+	<div className={styles.characterCell}>
+		<Avatar src={image} id={styles.darkLord} shape="circle" size={256} />
+		<div className={styles.description}>
+			<p>
+				<b>{name}: </b>
+				{description}
+			</p>
 		</div>
 	</div>
 );
