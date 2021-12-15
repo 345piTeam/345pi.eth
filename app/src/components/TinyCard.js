8ad98f045520db31be2b443772bb51f513de2c8f
nrgbistro <nolangelinas@gmail.com>
2021-12-15 18:15:08 -0500

    Prevent user from answering multiple times

--- app/src/components/TinyCard.js
+++ app/src/components/TinyCard.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import styles from "./css/TinyCard.module.css";
-import { Col, Row, List, Button, Image, Pagination } from "antd";
+import { List, Image, Pagination } from "antd";
 import { Link } from "react-router-dom";
 import q1Image from "../images/tiny-card-data/tc1/question1Image.png";
+import q2Image from "../images/tiny-card-data/tc1/question2Image.png";
 import AnswerButton from "./AnswerButton";
 
 const tinyCardData = [
@@ -18,81 +19,88 @@ const tinyCardData = [
 			String.raw`\int_{0}^{10}\pi\left(x\right)^{2}dx`,
 		],
 		answer: String.raw`\int_{0}^{10}\pi\left(x\right)^{2}dx`,
+		answered: false,
 	},
 	{
 		title: "TEST CARD 2",
+		image: q2Image,
 		question:
-			"It costs 200 345pi tokens to upgrade to a Duke OG. Modules cost only 0.5 tokens and you now have the ability to tip the top 5 Members on the leaderboard.",
+			"If y is a function of x such that y' > 0 for all x and y'' < 0 for all x, which of the following could be part of the graph of y = f(x)?",
+		options: ["A", "B", "C", "D", "E"],
+		answer: "B",
+		answered: false,
 	},
 	{
 		title: "TEST CARD 3",
+		answered: false,
 	},
 	{
 		title: "TEST CARD 4",
+		answered: false,
 	},
 ];
 
 const TinyCard = () => {
 	const [currentQuestion, setCurrentQuestion] = useState(1);
 	const [score, setScore] = useState(0);
+	const clickAnswerHandler = (e) => {
+		if (tinyCardData[currentQuestion - 1].answered) return;
+		if (e === tinyCardData[currentQuestion - 1].answer) {
+			setScore(score + 1);
+		}
+		tinyCardData[currentQuestion - 1].answered = true;
+		setCurrentQuestion(currentQuestion + 1);
+	};
 
-	const SingleCard = ({ title, question, image, options }) => {
-		const clickAnswerHandler = (e) => {
-			if (e === tinyCardData[currentQuestion - 1].answer) {
-				setScore(score + 1);
-			}
-			setCurrentQuestion(currentQuestion + 1);
-		};
-
-		return (
-			<div className={styles.singleCardContainer}>
-				<div className={styles.title}>
-					<h2>
-						Question {currentQuestion}: {title}
-					</h2>
-					<h2>{score}</h2>
-				</div>
-				<div className={styles.content}>
-					<Image src={image} width={400} />
-					<div className={styles.rightSide}>
-						<div className={styles.question}>
-							<div>{question}</div>
-						</div>
-						<div className={styles.answerBank}>
-							<List
-								grid={{
-									gutter: 16,
-									xs: 1,
-									sm: 2,
-									md: 2,
-									lg: 3,
-									xl: 4,
-									xxl: 3,
-								}}
-								dataSource={options}
-								renderItem={(item) => (
-									<List.Item>
-										<AnswerButton
-											content={item}
-											onAnswer={() => clickAnswerHandler(item)}
-										/>
-									</List.Item>
-								)}
-							/>
-						</div>
+	const SingleCard = ({ title, question, image, options, answered }) => (
+		<div className={styles.singleCardContainer}>
+			<div className={styles.title}>
+				<h2>
+					Question {currentQuestion}: {title}
+				</h2>
+				<h2>Score: {score}</h2>
+			</div>
+			<div className={styles.content}>
+				<Image src={image} width={400} />
+				<div className={styles.rightSide}>
+					<div className={styles.question}>
+						<div>{question}</div>
+					</div>
+					<div className={styles.answerBank}>
+						<List
+							grid={{
+								gutter: 16,
+								xs: 1,
+								sm: 2,
+								md: 2,
+								lg: 3,
+								xl: 4,
+								xxl: 3,
+							}}
+							dataSource={options}
+							renderItem={(item) => (
+								<List.Item>
+									<AnswerButton
+										content={item}
+										onAnswer={() => clickAnswerHandler(item)}
+										disabled={answered}
+									/>
+								</List.Item>
+							)}
+						/>
 					</div>
 				</div>
-				<footer className={styles.footer}>
-					<Pagination
-						current={currentQuestion}
-						onChange={setCurrentQuestion}
-						responsive={true}
-						total={10 * tinyCardData.length}
-					/>
-				</footer>
 			</div>
-		);
-	};
+			<footer className={styles.footer}>
+				<Pagination
+					current={currentQuestion}
+					onChange={setCurrentQuestion}
+					responsive={true}
+					total={10 * tinyCardData.length}
+				/>
+			</footer>
+		</div>
+	);
 
 	return (
 		<div className={styles.tinyCardContainer}>
@@ -101,6 +109,7 @@ const TinyCard = () => {
 				question={tinyCardData[currentQuestion - 1].question}
 				image={tinyCardData[currentQuestion - 1].image}
 				options={tinyCardData[currentQuestion - 1].options}
+				answered={tinyCardData[currentQuestion - 1].answered}
 			/>
 		</div>
 	);
