e350a15bdacc401c902ccebc3610d5c5b09b56a8
Nolan Gelinas <nolan.gelinas@duke.edu>
2022-02-15 17:25:33 -0500

    Create simple hardhat project and move contents of app to root

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import "./App.css";
+import React from "react";
+import Navbar from "./components/Navbar";
+//import Governance from "./pages/Governance";
+import LoadingSpinner from "./components/LoadingSpinner";
+import Homepage from "./pages/Homepage.js";
+//import Marketplace from "./pages/Marketplace";
+//import TinyCardPage from "./pages/TinyCardPage";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "antd/dist/antd.css";
+
+const App = () => (
+	<React.StrictMode>
+		<BrowserRouter>
+			<Navbar />
+			<Routes>
+				<Route strict path={"/"} element={<Homepage />}></Route>
+				{/* <Route path={"/modules"} element={<TinyCardPage />}></Route>
+						<Route
+							path={"/governance"}
+							element={initialized ? <Governance /> : <LoadingSpinner />}
+						></Route> */}
+				<Route path={"*"} element={<PageNotFound />}></Route>
+			</Routes>
+		</BrowserRouter>
+	</React.StrictMode>
+);
+
+const PageNotFound = () => (
+	<div>
+		<h1>Page Not Found!</h1>
+	</div>
+);
+
+export default App;
