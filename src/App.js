a9bc830f70396bf8ec299f9b29c395eee2c5aa55
Nolan Gelinas <nolangelinas@gmail.com>
2022-02-27 16:14:52 -0500

    Add wallet connection button to navbar

--- src/App.js
+++ src/App.js
@@ -1,30 +1,45 @@
 import "./App.css";
 import React from "react";
+import { useEffect } from "react";
+import { useMoralis } from "react-moralis";
 import Navbar from "./components/Navbar";
 //import Governance from "./pages/Governance";
-import LoadingSpinner from "./components/LoadingSpinner";
 import Homepage from "./pages/Homepage.js";
 //import Marketplace from "./pages/Marketplace";
 //import TinyCardPage from "./pages/TinyCardPage";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "antd/dist/antd.css";
+import { MoralisProvider } from "react-moralis";
 
-const App = () => (
-	<React.StrictMode>
+const APP_ID = process.env.REACT_APP_MORALIS_APPLICATION_ID;
+const SERVER_URL = process.env.REACT_APP_MORALIS_SERVER_URL;
+
+const App = () => {
+	const { isWeb3Enabled, enableWeb3, isAuthenticated, isWeb3EnableLoading } =
+		useMoralis();
+
+	useEffect(() => {
+		const connectorId = window.localStorage.getItem("connectorId");
+		if (isAuthenticated && !isWeb3Enabled && !isWeb3EnableLoading)
+			enableWeb3({ provider: connectorId });
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [isAuthenticated, isWeb3Enabled]);
+
+	return (
 		<BrowserRouter>
 			<Navbar />
 			<Routes>
 				<Route strict path={"/"} element={<Homepage />}></Route>
 				{/* <Route path={"/modules"} element={<TinyCardPage />}></Route>
-						<Route
-							path={"/governance"}
-							element={initialized ? <Governance /> : <LoadingSpinner />}
-						></Route> */}
+					<Route
+						path={"/governance"}
+						element={initialized ? <Governance /> : <LoadingSpinner />}
+					></Route> */}
 				<Route path={"*"} element={<PageNotFound />}></Route>
 			</Routes>
 		</BrowserRouter>
-	</React.StrictMode>
-);
+	);
+};
 
 const PageNotFound = () => (
 	<div>
@@ -32,4 +47,10 @@ const PageNotFound = () => (
 	</div>
 );
 
-export default App;
+const AppContainer = () => (
+	<MoralisProvider appId={APP_ID} serverUrl={SERVER_URL}>
+		<App />
+	</MoralisProvider>
+);
+
+export default AppContainer;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,50 +2,15 @@ import React from "react";
 import styles from "./css/Navbar.module.css";
 import logoImg from "../images/345piUsLogo_H.png";
 import { Link, NavLink } from "react-router-dom";
-import { CheckCircleOutlined, CloseCircleOutlined } from "@ant-design/icons";
 import { Tooltip } from "antd";
+import Account from "./Account/Account.jsx";
 
 const Navbar = () => {
 	const NavbarButton = ({ name, link }) => (
-		<NavLink to={link} activeClassName="active">
+		<NavLink to={link} activeclassname="active">
 			<div className={styles.navButton}>{name}</div>
 		</NavLink>
 	);
-
-	const WalletDisconnected = () => (
-		<Tooltip
-			title={<span style={{ fontSize: "24px" }}>Wallet Disconnected</span>}
-			placement="bottomRight"
-			color="#c43636"
-			arrowPointAtCenter
-		>
-			<div className={styles.walletStatus}>
-				<CloseCircleOutlined
-					id={styles.walletDisconnected}
-					className={styles.walletStatusIcon}
-				/>
-			</div>
-		</Tooltip>
-	);
-
-	const WalletConnected = () => (
-		<Tooltip
-			title={<span style={{ fontSize: "24px" }}>Wallet Connected</span>}
-			placement="bottomRight"
-			color="#3aa859"
-			arrowPointAtCenter
-		>
-			<div className={styles.walletStatus}>
-				<CheckCircleOutlined
-					id={styles.walletConnected}
-					className={styles.walletStatusIcon}
-				/>
-				<p className={styles.walletStatusMessage}>Wallet Connected</p>
-			</div>
-		</Tooltip>
-	);
-
-	let initialized = false;
 
 	return (
 		<div className={styles.navbar}>
@@ -61,9 +26,7 @@ const Navbar = () => {
 				<NavbarButton name="Calculus Modules" link="/modules" />
 				<NavbarButton name="Governance" link="/governance" />
 			</div>
-			<div className={styles.walletStatusContainer}>
-				{initialized ? <WalletConnected /> : <WalletDisconnected />}
-			</div>
+			<Account />
 		</div>
 	);
 };
