98d4daf4dd4da6a19754d2eed1bdd8d2ef746ba6
Nolan Gelinas <nolangelinas@gmail.com>
2022-02-27 17:45:28 -0500

    TODO: Add environment secretes to github-ci

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import { MoralisProvider } from "react-moralis";
 const APP_ID = process.env.REACT_APP_MORALIS_APPLICATION_ID;
 const SERVER_URL = process.env.REACT_APP_MORALIS_SERVER_URL;
 
-const App = () => {
+export const App = () => {
 	const { isWeb3Enabled, enableWeb3, isAuthenticated, isWeb3EnableLoading } =
 		useMoralis();
 
