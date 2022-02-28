cb1debaffd874ea4a434e0ea2de4183dcee05293
Nolan Gelinas <nolangelinas@gmail.com>
2022-02-27 22:48:55 -0500

    Create DarkLord from contract wizard

--- contracts/DarkLord.sol
+++ contracts/DarkLord.sol
@@ -1,26 +1,47 @@
-//SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
 
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+// FOR DEVELOPMENT
 import "hardhat/console.sol";
 
-contract DarkLord {
-    address private owner;
+/// @custom:security-contact nolan@345pi.com
+contract DarkLord is ERC721, ERC721URIStorage, Ownable {
+    using Counters for Counters.Counter;
 
-    constructor() {
-        owner = msg.sender;
+    mapping(address => bool) private owners;
+
+    Counters.Counter private _tokenIdCounter;
+
+    constructor() ERC721("Dark Lord", "DKL") {}
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "ipfs://";
     }
 
-    function getOwner() public view returns (address) {
-        return owner;
+    function safeMint(address to, string memory uri) public onlyOwner {
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+        owners[to] = true;
     }
 
-    function setOwner(address _owner) public onlyOwner {
-        console.log("Changing owner from '%s' to '%s'", owner, _owner);
-        owner = _owner;
+    // The following functions are overrides required by Solidity.
+
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
     }
 
-    modifier onlyOwner {
-      require(msg.sender == owner, "ERROR: You are not the owner of this NFT!");
-      _;
-   }
+    function tokenURI(uint256 tokenId) public view override(ERC721, ERC721URIStorage) returns (string memory) {
+        return super.tokenURI(tokenId);
+    }
+
+    function isOwner(address a) public view returns (bool) {
+        return owners[a];
+    }
 }
--- test/DarkLord.js
+++ test/DarkLord.js
@@ -2,16 +2,18 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("DarkLord", function () {
-	let owner, darkLord;
+	let owner, addr1, addr2, addr3, darkLord;
 
 	beforeEach(async () => {
-		[owner] = await ethers.getSigners();
+		[owner, addr1, addr2, addr3] = await ethers.getSigners();
 		const DarkLord = await ethers.getContractFactory("DarkLord");
 		darkLord = await DarkLord.deploy();
 		await darkLord.deployed();
+		darkLord.safeMint(owner.address, "1");
 	});
 
 	it("Should set the owner to the contract creator", async function () {
-		expect(await darkLord.getOwner()).to.equal(owner.address);
+		expect(await darkLord.isOwner(addr1.address)).to.equal(false);
+		expect(await darkLord.isOwner(owner.address)).to.equal(true);
 	});
 });
