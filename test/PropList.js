c6a506d034eba876da03d62f2c1638f14d6e4a1b
Aryan-Chaudhary0220 <aryan.chaudhary@duke.edu>
2022-03-01 23:00:06 -0500

    Updated tests for ProposalList

--- test/PropList.js
+++ test/PropList.js
@@ -7,7 +7,6 @@ describe("Proposal List", function () {
 		// Get the ContractFactory and Signers here.
 		Prop = await ethers.getContractFactory("ProposalList");
 		[owner, addr1, addr2, ...addrs] = await ethers.getSigners();
-
 		// To deploy our contract, we just have to call Token.deploy() and await
 		// for it to be deployed(), which happens once its transaction has been
 		// mined.
@@ -19,7 +18,6 @@ describe("Proposal List", function () {
 	describe("Deployment", function () {
 		// `it` is another Mocha function. This is the one you use to define your
 		// tests. It receives the test name, and a callback function.
-
 		it("Initializes with two proposals", function () {
 			return PropList.deployed()
 				.then(function (instance) {
@@ -29,7 +27,6 @@ describe("Proposal List", function () {
 					expect(count).to.equal(2);
 				});
 		});
-
 		it("Initializes the proposals with the correct values", function () {
 			return PropList.deployed()
 				.then(function (instance) {
@@ -46,16 +43,13 @@ describe("Proposal List", function () {
 					expect(proposal.name).to.equal("Other Proposal");
 				});
 		});
-		// it("Initializes the options with the correct values", async function () {
-		// 	return await PropList.deployed()
-		// 		.then(function (instance) {
-		// 			proposalInstance = instance;
-		// 			return proposalInstance;
-		// 		})
-		// 		.then(async function (proposal) {
-		// 			expect(proposal.getOptionName(0, 0)).to.equal("Click-Based");
-		// 		});
-		// });
+		it("Initializes the options with the correct values", async function () {
+            const optionName = await PropList.getOptionName(0, 0);
+            const optionSummary = await PropList.getOptionSummary(0, 0); 
+            expect(optionName).to.equal("Click-Based");
+            expect(optionSummary).to.equal("Primarily Multiple Choice and Mouse Games");
+		});
+		
 	});
 
 	describe("Voting", function () {
@@ -66,13 +60,11 @@ describe("Proposal List", function () {
 			await darkLord.safeMint(owner.address, "0");
 			await PropList.setDarkLordAddress(darkLord.address);
 		});
-
 		it("Dark Lord Casts Vote", async function () {
 			await PropList.vote(0, 0);
 			const voteCount = await PropList.getOptionVoteCount(0, 0);
 			expect(voteCount).to.equal(7);
 		});
-
 		it("Cannot Cast vote if not Dark Lord", async function () {
 			await PropList.connect(addr1).vote(0, 0);
 			const voteCount = await PropList.getOptionVoteCount(0, 0);
