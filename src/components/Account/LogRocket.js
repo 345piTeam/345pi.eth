f9bb1f745387cb08469dd979277b846f845b5bd2
Nolan Gelinas <nolan.gelinas@duke.edu>
2022-03-06 23:04:51 -0500

    Add log.rocket

---
+++ src/components/Account/LogRocket.js
@@ -0,0 +1,17 @@
+import LogRocket from "logrocket";
+import setupLogRocketReact from "logrocket-react";
+LogRocket.init("nirrdk/345pi");
+
+async function connectToLogRocket(address) {
+	// This is an example script - don't forget to change it!
+	LogRocket.identify(address, {
+		name: null, // Add function to get the eth name of an address
+	});
+
+	// after calling LogRocket.init()
+	setupLogRocketReact(LogRocket);
+
+	console.log("user " + address + " connected to log.rocket");
+}
+
+export default connectToLogRocket;
