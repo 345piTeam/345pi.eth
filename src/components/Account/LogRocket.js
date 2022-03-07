4a74b97596eb31ca71698d02b48664c8a29e3bda
Nolan Gelinas <nolan.gelinas@duke.edu>
2022-03-06 23:25:56 -0500

    Remove logging and Fix multiple init of logRocket

--- src/components/Account/LogRocket.js
+++ src/components/Account/LogRocket.js
@@ -2,10 +2,6 @@ import LogRocket from "logrocket";
 import setupLogRocketReact from "logrocket-react";
 
 async function connectToLogRocket(address) {
-	console.log(window.XMLHttpRequest);
-	if (window.XMLHttpRequest) {
-		LogRocket.init("nirrdk/345pi");
-	}
 	// This is an example script - don't forget to change it!
 	LogRocket.identify(address, {
 		name: null, // Add function to get the eth name of an address
@@ -13,8 +9,6 @@ async function connectToLogRocket(address) {
 
 	// after calling LogRocket.init()
 	setupLogRocketReact(LogRocket);
-
-	console.log("user " + address + " connected to log.rocket");
 }
 
 export default connectToLogRocket;
