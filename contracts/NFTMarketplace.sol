2fb9e23bd509066c9a741196b6395f4761fa3948
Aryan-Chaudhary0220 <aryan.chaudhary@duke.edu>
2022-03-09 00:07:09 -0500

    Updated NFTMarketplace

--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -1,154 +1,67 @@
-// SPDX-License-Identifier: MIT OR Apache-2.0
-pragma solidity ^0.8.4;
+pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/utils/Counters.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-
-contract NFT is ERC721URIStorage {
-    using Counters for Counters.Counter;
-    Counters.Counter private _tokenIds;
-    address contractAddress;
-
-    constructor(address marketplaceAddress) ERC721("Nader's Digital Marketplace", "NDM") {
-        contractAddress = marketplaceAddress;
-    }
-
-    function createToken(string memory tokenURI) public returns (uint) {
-        _tokenIds.increment();
-        uint256 newItemId = _tokenIds.current();
-
-        _mint(msg.sender, newItemId);
-        _setTokenURI(newItemId, tokenURI);
-        setApprovalForAll(contractAddress, true);
-        return newItemId;
-    }
+interface IDepositContract {
+    function deposit(
+        bytes calldata pubkey,
+        bytes calldata withdrawal_credentials,
+        bytes calldata signature,
+        bytes32 deposit_data_root
+    ) external payable;
 }
 
-contract NFTMarket is ReentrancyGuard {
-  using Counters for Counters.Counter;
-  Counters.Counter private _itemIds;
-  Counters.Counter private _itemsSold;
-
-  address payable owner;
-  uint256 listingPrice = 0.1 ether;
-
-  constructor() {
-    owner = payable(msg.sender);
-  }
-
-  struct MarketItem {
-    uint itemId;
-    address nftContract;
-    uint256 tokenId;
-    address payable seller;
-    address payable owner;
-    uint256 price;
-  }
-
-  mapping(uint256 => MarketItem) private idToMarketItem;
-
-  event MarketItemCreated (
-    uint indexed itemId,
-    address indexed nftContract,
-    uint256 indexed tokenId,
-    address seller,
-    address owner,
-    uint256 price
-  );
-
-  function getMarketItem(uint256 marketItemId) public view returns (MarketItem memory) {
-    return idToMarketItem[marketItemId];
-  }
-
-  function createMarketItem(
-    address nftContract,
-    uint256 tokenId,
-    uint256 price
-  ) public payable nonReentrant {
-    require(price > 0, "Price must be at least 1 wei");
-    require(msg.value == listingPrice, "Price must be equal to listing price");
-
-    _itemIds.increment();
-    uint256 itemId = _itemIds.current();
-  
-    idToMarketItem[itemId] =  MarketItem(
-      itemId,
-      nftContract,
-      tokenId,
-      payable(msg.sender),
-      payable(address(0)),
-      price
+contract StakingPool {
+    mapping(address => uint) public balances;
+    mapping(bytes => bool) public pubkeysUsed;
+    IDepositContract public depositContract = IDepositContract(0x00000000219ab540356cBB839Cbe05303d7705Fa);
+    address public admin;
+    uint public end;
+    bool public finalized;
+    uint public totalInvested;
+    uint public totalChange;
+    mapping(address => bool) public changeClaimed;
+    
+    event NewInvestor (
+        address investor
     );
 
-    IERC721(nftContract).transferFrom(msg.sender, address(this), tokenId);
-
-    emit MarketItemCreated(
-      itemId,
-      nftContract,
-      tokenId,
-      msg.sender,
-      address(0),
-      price
-    );
-  }
-
-  function createMarketSale(
-    address nftContract,
-    uint256 itemId
-    ) public payable nonReentrant {
-    uint price = idToMarketItem[itemId].price;
-    uint tokenId = idToMarketItem[itemId].tokenId;
-    require(msg.value == price, "Please submit the asking price in order to complete the purchase");
-
-    idToMarketItem[itemId].seller.transfer(msg.value);
-    IERC721(nftContract).transferFrom(address(this), msg.sender, tokenId);
-    idToMarketItem[itemId].owner = payable(msg.sender);
-    _itemsSold.increment();
-    payable(owner).transfer(listingPrice);
-  }
-
-  function fetchMarketItems() public view returns (MarketItem[] memory) {
-    uint itemCount = _itemIds.current();
-    uint unsoldItemCount = _itemIds.current() - _itemsSold.current();
-    uint currentIndex = 0;
-
-    MarketItem[] memory items = new MarketItem[](unsoldItemCount);
-    for (uint i = 0; i < itemCount; i++) {
-      if (idToMarketItem[i + 1].owner == address(0)) {
-        uint currentId = i + 1;
-        MarketItem storage currentItem = idToMarketItem[currentId];
-        items[currentIndex] = currentItem;
-        currentIndex += 1;
-      }
+    constructor() {
+        admin = msg.sender;
+        end = block.timestamp + 7 days;
     }
-   
-    return items;
-  }
-
-  function fetchMyNFTs() public view returns (MarketItem[] memory) {
-    uint totalItemCount = _itemIds.current();
-    uint itemCount = 0;
-    uint currentIndex = 0;
-
-    for (uint i = 0; i < totalItemCount; i++) {
-      if (idToMarketItem[i + 1].owner == msg.sender) {
-        itemCount += 1;
-      }
+    
+    function invest() external payable {
+        require(block.timestamp < end, 'too late');
+        if(balances[msg.sender] == 0) {
+            emit NewInvestor(msg.sender);   
+        }
+        balances[msg.sender] += msg.value;
     }
 
-    MarketItem[] memory items = new MarketItem[](itemCount);
-    for (uint i = 0; i < totalItemCount; i++) {
-      if (idToMarketItem[i + 1].owner == msg.sender) {
-        uint currentId = i + 1;
-        MarketItem storage currentItem = idToMarketItem[currentId];
-        items[currentIndex] = currentItem;
-        currentIndex += 1;
-      }
+    function finalize() external {
+      require(block.timestamp >= end, 'too early');
+      require(finalized == false, 'already finalized');
+      finalized = true;
+      totalInvested = address(this).balance;
+      totalChange = address(this).balance % 32 ether;
     }
-   
-    return items;
-  }
+    
+    function deposit(
+        bytes calldata pubkey,
+        bytes calldata withdrawal_credentials,
+        bytes calldata signature,
+        bytes32 deposit_data_root
+    )
+        external
+    {
+        require(finalized == true, 'too early');
+        require(msg.sender == admin, 'only admin');
+        require(address(this).balance >= 32 ether);
+        require(pubkeysUsed[pubkey] == false, 'this pubkey was already used');
+        depositContract.deposit{value: 32 ether}(
+            pubkey, 
+            withdrawal_credentials, 
+            signature, 
+            deposit_data_root
+        );
+    }
 }
-Load
