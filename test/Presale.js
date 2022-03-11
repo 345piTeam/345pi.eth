678bdd5bb262073ffa317ec56b7db46295e7ae9c
Nolan Gelinas <nolan.gelinas@duke.edu>
2022-03-11 01:16:36 -0500

    Attempt to fix contract tests

--- test/Presale.js
+++ test/Presale.js
@@ -8,31 +8,30 @@ describe("Presale", function () {
 		Pre = await ethers.getContractFactory("Presale");
 		[owner, addr1, addr2, ...addrs] = await ethers.getSigners();
 		Presale = await Pre.deploy();
-        await Presale.deployed();
-        
-        const Wizard = await ethers.getContractFactory("Wizard");
-        wizard = await Wizard.deploy();
-        await wizard.deployed();
-        console.log(wizard.address);
-        await wizard.safeMint(Presale.address, "0");
-        //console.log(wizard.address);
-    });
-    describe("Presale Investing", function () {
-        it("Added to investor list", async function () {
-            await Presale.connect(addr1).invest(5);
-            console.log(Presale.isInvestor);
+		await Presale.deployed();
+
+		const Wizard = await ethers.getContractFactory("Wizard");
+		wizard = await Wizard.deploy();
+		await wizard.deployed();
+		console.log(wizard.address);
+		await wizard.safeMint(Presale.address, "0");
+	});
+	describe("Presale Investing", function () {
+		it("Added to investor list", async function () {
+			await Presale.connect(addr1).invest(5);
+			console.log(Presale.isInvestor);
 			expect(await Presale.isInvested(addr1.address)).to.equal(true);
-        });
-        it("A Non inestor is not on the list", async function () {
-            await Presale.connect(addr1).invest(5);
-            console.log(Presale.isInvestor);
-			expect(await Presale.isInvested(addr2.address)).to.equal(false);
-        });
-        it("Added to contract balance", async function () {
-            const before = await address(Presale).balance;
-            await Presale.connect(addr2).invest(5);
-            const after = await address(PreSale).balance;
-			expect(after-before).to.equal(0.005);
 		});
-    });
+		it("A Non investor is not on the list", async function () {
+			await Presale.connect(addr1).invest(5);
+			//console.log(Presale.isInvestor);
+			expect(await Presale.isInvested(addr2.address)).to.equal(false);
+		});
+		it("Added to contract balance", async function () {
+			const before = await Presale.balance;
+			await Presale.connect(addr2).invest(5);
+			const after = await PreSale.balance;
+			expect(after - before).to.equal(0.005);
+		});
+	});
 });
